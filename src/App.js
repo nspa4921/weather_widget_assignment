ee93579f084cd2c535c717d6c3ec9822c4ef059c
Nemanja Spasic <nemo.spaske@live.com>
2021-07-02 11:04:14 +0200

    axion installed + API called

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import Axios from "axios";
+
 import './App.css';
 
 
@@ -13,16 +15,36 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let newCoords = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      }
+    // get device location
+    if (navigator.geolocation) {
+      
+      navigator.geolocation.getCurrentPosition((position) => {
+        let newCoords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
 
-      this.setState({coords:newCoords})
+        this.setState({coords:newCoords});
 
-    })
-  }
+               //API Call
+               Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Copenhagen,dk&units=metric&appid=166d00e26d3ff2c6149e89feccc5c59a&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res =>{
+          
+                let weatherData = {
+                  location: res.data.name,
+                  temperature: res.data.main.temp,
+                  humidity: res.data.main.humidity,
+                  wind: res.data.wind.deg,
+                  wind_speed: res.data.wind.speed,
+                }
+      
+                this.setState({data:weatherData});
+                console.log(res)
+              })
+            })
+          } else {
+            console.log("not supported")
+          }
+        }
 
     render() {
       return (
