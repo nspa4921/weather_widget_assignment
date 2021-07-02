06d1d606bca97421b34551c80eed17e440df31ba
Nemanja Spasic <nemo.spaske@live.com>
2021-07-02 10:55:53 +0200

    device location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Weather widget
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    coords:{
+      latitude: 45,
+      longitude: 60
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let newCoords = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+
+      this.setState({coords:newCoords})
+
+    })
+  }
+
+    render() {
+      return (
+        <div className="App">
+            Weather widget
+        </div>
+      );
+    }
+  
+  
 }
 
 export default App;
